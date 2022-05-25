2413aff5feeb4fae912647d69094658ba9bb3e76
Name <email@example.ru>
2022-05-25 23:31:31 +0300

    Add files via upload

--- NewsPaper/protect/urls.py
+++ NewsPaper/protect/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import IndexView
 
-# перенаправляемся на единственное представление IndexView, которое описано в соответствующем файле views.py
 urlpatterns = [
     path('', IndexView.as_view()),
 ]
